9e65fd5aa5ca077dfbc84b7b75ecc403edc121ac
Sebastian <sebastian.degerman@consid.se>
2023-10-25 14:27:18 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+## Licensing
+
+- Copyright 2022 [Creative Tim](https://www.creative-tim.com?ref=readme-mtkr)
+- Creative Tim [license](https://www.creative-tim.com/license?ref=readme-mtkr)
+
+## Useful Links
+
+- [More products](https://www.creative-tim.com/templates?ref=readme-mtkr) from Creative Tim
+
+- [Tutorials](https://www.youtube.com/channel/UCVyTG4sCw-rOvB9oHkzZD1w)
+
+- [Freebies](https://www.creative-tim.com/bootstrap-themes/free?ref=readme-mtkr) from Creative Tim
+
+- [Affiliate Program](https://www.creative-tim.com/affiliates/new?ref=readme-mtkr) (earn money)
---
+++ src/data/team-data.js
@@ -0,0 +1,80 @@
+export const teamData = [
+  {
+    img: "/img/team-1.jpg",
+    name: "Superio Marioso",
+    position: "Frontend Developer",
+    socials: [
+      {
+        color: "light-blue",
+        name: "twitter",
+      },
+      {
+        color: "blue",
+        name: "facebook",
+      },
+      {
+        color: "pink",
+        name: "dribbble",
+      },
+    ],
+  },
+  {
+    img: "/img/team-2.jpg",
+    name: "Anakin Skywalker af Vader",
+    position: "Frontend Developer",
+    socials: [
+      {
+        color: "light-blue",
+        name: "twitter",
+      },
+      {
+        color: "blue",
+        name: "facebook",
+      },
+      {
+        color: "pink",
+        name: "dribbble",
+      },
+    ],
+  },
+  {
+    img: "/img/team-3.jpg",
+    name: "Irony Mansson",
+    position: "UI/UX Designer",
+    socials: [
+      {
+        color: "light-blue",
+        name: "twitter",
+      },
+      {
+        color: "blue",
+        name: "facebook",
+      },
+      {
+        color: "pink",
+        name: "dribbble",
+      },
+    ],
+  },
+  {
+    img: "/img/team-4.jpg",
+    name: "Bruce Wayne",
+    position: "Backend Developer",
+    socials: [
+      {
+        color: "light-blue",
+        name: "twitter",
+      },
+      {
+        color: "blue",
+        name: "facebook",
+      },
+      {
+        color: "pink",
+        name: "dribbble",
+      },
+    ],
+  },
+];
+
+export default teamData;
